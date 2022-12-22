1ea8ae94565976cb53adbdc2bd9fe7c311e6e203
Martin <88441320+MartoIT@users.noreply.github.com>
2022-12-22 15:53:14 +0200

    Delete clouds.js

--- clouds.js
+++
@@ -1,9 +0,0 @@
-
-
-export function createClouds(stateClouds) {
-    const cloud = document.createElement('div');
-    cloud.classList.add('cloud');
-    cloud.style.top = stateClouds.currentCloud.x + 'px';
-    cloud.style.left = stateClouds.currentCloud.y + 'px';
-    return cloud;
-}
